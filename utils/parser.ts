b6a41e4da0b16a4234c79d56e6fd731315911e53
Matthew Leon <ml@matthewleon.com>
2021-11-25 21:57:42 -0500

    fix lint issues

--- utils/parser.ts
+++ utils/parser.ts
@@ -379,11 +379,13 @@ export class Parser {
           case (this.#options.reviveBooleans ?? true) && /^(?:true|false)$/i.test(value):
             return /^true$/i.test(value);
           //Revive numbers
-          case this.#options.reviveNumbers ?? true:
+          case this.#options.reviveNumbers ?? true: {
             const num = Number(value);
             if (Number.isFinite(num)) {
               return num;
             }
+          }
+          /* falls through */
           //Strings
           default:
             //Unescape XML entities
