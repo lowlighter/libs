d7ff208afae990f24d1565af99bbef87ae9f30bc
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2024-02-23 09:12:37 +0100

    feat: add diff patience library

---
+++ diff_test.ts
@@ -0,0 +1,72 @@
+import { diff } from "./diff.ts"
+import { expect } from "std/expect/expect.ts"
+
+/** Read test content */
+async function read(test: string) {
+  const a = await Deno.readTextFile(new URL(`testing/diff/${test}/a`, import.meta.url))
+  const b = await Deno.readTextFile(new URL(`testing/diff/${test}/b`, import.meta.url))
+  const c = await Deno.readTextFile(new URL(`testing/diff/${test}/c`, import.meta.url))
+  return { a: a.replaceAll("\r\n", "\n"), b: b.replaceAll("\r\n", "\n"), c: c.replaceAll("\r\n", "\n") }
+}
+
+// Tests ——————————————————————————————————————————————————————————————————————————————————————————
+
+Deno.test("diff() handles empty texts", { permissions: { read: true } }, async () => {
+  const { a, b, c } = await read("empty")
+  expect(diff(a, b)).toStrictEqual(c)
+})
+
+Deno.test("diff() handles empty unchanged texts", { permissions: { read: true } }, async () => {
+  const { a, b, c } = await read("identical")
+  expect(diff(a, b)).toStrictEqual(c)
+})
+
+Deno.test("diff() handles final newline in texts", { permissions: { read: true } }, async () => {
+  const { a, b, c } = await read("newline")
+  expect(diff(a, b)).toStrictEqual(c)
+})
+
+Deno.test("diff() handles single addition", { permissions: { read: true } }, async () => {
+  const { a, b, c } = await read("single/addition")
+  expect(diff(a, b)).toStrictEqual(c)
+})
+
+Deno.test("diff() handles additions", { permissions: { read: true } }, async () => {
+  const { a, b, c } = await read("addition")
+  expect(diff(a, b)).toStrictEqual(c)
+})
+
+Deno.test("diff() handles single deletion", { permissions: { read: true } }, async () => {
+  const { a, b, c } = await read("single/deletion")
+  expect(diff(a, b)).toStrictEqual(c)
+})
+
+Deno.test("diff() handles deletions", { permissions: { read: true } }, async () => {
+  const { a, b, c } = await read("deletion")
+  expect(diff(a, b)).toStrictEqual(c)
+})
+
+Deno.test("diff() handles single edition", { permissions: { read: true } }, async () => {
+  const { a, b, c } = await read("single/edition")
+  expect(diff(a, b)).toStrictEqual(c)
+})
+
+Deno.test("diff() handles editions", { permissions: { read: true } }, async () => {
+  const { a, b, c } = await read("edition")
+  expect(diff(a, b)).toStrictEqual(c)
+})
+
+Deno.test("diff() handles multiple hunks", { permissions: { read: true } }, async () => {
+  const { a, b, c } = await read("hunks")
+  expect(diff(a, b)).toStrictEqual(c)
+})
+
+Deno.test("diff() handles moved lines", { permissions: { read: true } }, async () => {
+  const { a, b, c } = await read("moved")
+  expect(diff(a, b)).toStrictEqual(c)
+})
+
+Deno.test("diff() handles complex texts", { permissions: { read: true } }, async () => {
+  const { a, b, c } = await read("lorem")
+  expect(diff(a, b)).toStrictEqual(c)
+})
