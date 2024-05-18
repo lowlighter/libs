7c5a8569a15c8c15cc02f9dd530ca681ab5fe0bd
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2024-05-18 01:36:48 +0000

    continue

--- wasm_xml_parser/build.ts
+++ wasm_xml_parser/build.ts
@@ -1,25 +1,2 @@
-import { fromFileUrl } from "jsr:@std/path/from-file-url"
-import { encodeBase64 } from "jsr:@std/encoding/base64"
-import {bgCyan} from "jsr:@std/fmt/colors"
-import {bundle} from "jsr:@libs/bundle/typescript"
-
-const name = "wasm_xml_parser"
-
-console.log(bgCyan("wasm build".padEnd(48)))
-const command = new Deno.Command("wasm-pack", {args:["build", "--release", "--target", "web"], cwd:import.meta.dirname, stdin:"inherit", stdout:"inherit", stderr:"inherit"})
-const {success} = await command.output()
-if (!success)
-    throw new Error("wasm build failed")
-
-console.log(bgCyan("inject base64 wasm to js".padEnd(48)))
-const wasm = await fetch(import.meta.resolve(`./pkg/${name}_bg.wasm`)).then(response => response.arrayBuffer())
-const js = await fetch(import.meta.resolve(`./pkg/${name}.js`)).then(response => response.text())
-if (!js.includes(`'${name}_bg.wasm'`))
-    throw new Error("failed to find injection location")
-await Deno.writeTextFile(fromFileUrl(import.meta.resolve(`./${name}.js`)), js.replace(`'${name}_bg.wasm'`, `'data:application/wasm;base64,${encodeBase64(wasm)}'`))
-console.log("ok")
-
-console.log(bgCyan("minify js".padEnd(48)))
-const minified = await bundle(new URL(import.meta.resolve(`./${name}.js`)), {minify:"terser", banner:"WASM XML parser - https://github.com/lowlighter/xml"})
-await Deno.writeTextFile(fromFileUrl(import.meta.resolve(`./${name}.js`)), minified)
-console.log(`size: ${new Blob([minified]).size}b`)
+import {bundle} from "jsr:@libs/bundle@3.0.1/wasm"
+await bundle("wasm_xml_parser", {cwd:import.meta.dirname!, banner:"https://github.com/lowlighter/libs"})
