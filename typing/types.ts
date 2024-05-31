a1e2372c95e08c291cc59562b05d88d58b544ac6
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2024-05-31 23:51:18 +0200

    refactor(typing): normalize exports

---
+++ typing/types.ts
@@ -0,0 +1,44 @@
+/** Read-write accessor. */
+//deno-lint-ignore no-explicit-any
+export type rw = any
+
+/** Callback function. */
+// deno-lint-ignore ban-types
+export type callback = Function
+
+/** Record. */
+export type record<T = unknown> = Record<PropertyKey, T>
+
+/** Extract key type from {@link https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Map | Map}. */
+export type MapKey<T> = T extends Map<infer U, unknown> ? U : never
+
+/** Extract value type from {@link https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Map | Map}. */
+export type MapValue<T> = T extends Map<unknown, infer U> ? U : never
+
+/** Extract value type from {@link https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Set | Set}. */
+export type SetValue<T> = T extends Set<infer U> ? U : never
+
+/** Extract key type from {@link https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/WeakMap | WeakMap}. */
+export type WeakMapKey<T> = T extends WeakMap<infer U, unknown> ? U : never
+
+/** Extract value type from {@link https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/WeakMap | WeakMap}. */
+export type WeakMapValue<T> = T extends WeakMap<WeakKey, infer U> ? U : never
+
+/** Extract value type from {@link https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/WeakSet | WeakSet}. */
+export type WeakSetValue<T> = T extends WeakSet<infer U> ? U : never
+
+/** Optional type. */
+export type Optional<T> = T | undefined
+
+/** Nullable type. */
+export type Nullable<T> = T | null
+
+/** Promisable type.  */
+export type Promisable<T> = T | Promise<T>
+
+/** Arrayable type. */
+export type Arrayable<T> = T | Array<T>
+
+/** Extract function argument. */
+// deno-lint-ignore no-explicit-any
+export type Arg<T extends ((...args: any[]) => any), index extends number = 0, required extends boolean = false> = required extends true ? NonNullable<Parameters<T>[index]> : Parameters<T>[index]
