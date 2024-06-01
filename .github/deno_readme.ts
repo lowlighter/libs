f166bb0da4aafc4affcec4af96f7f93a8b4c2945
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2024-06-01 22:39:16 +0200

    doc: update readme

--- .github/deno_readme.ts
+++ .github/deno_readme.ts
@@ -27,7 +27,7 @@ for await (const { path } of expandGlob(`*/deno.jsonc`, { root })) {
     <td rowspan="4">${features}</td>
   </tr>
   <tr><th>${npm ? `<a href="https://www.npmjs.com/package/@lowlighter/${name}"><img src="https://img.shields.io/npm/v/@lowlighter%2F${name}?logo=npm&labelColor=cb0000&color=183e4e"></a>` : ""}</th></tr>
-  <tr><th>${playground ? `<a href="${playground}"><img src="https://img.shields.io/badge/Playground--black?style=flat&logo=deno&labelColor=black"></a>` : ""}</th></tr>
+  <tr><th>${playground ? `<a href="${playground}"><img src="https://img.shields.io/badge/Playground--black?style=flat&logo=windowsterminal&labelColor=black"></a>` : ""}</th></tr>
   <tr><th><a href="https://libs-coverage.lecoq.io/${name}"><img src="https://libs-coverage.lecoq.io/${name}/badge.svg"></a></th></tr>
   `.trim()
 }
