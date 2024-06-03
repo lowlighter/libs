3602e579ed77d40cef18156a427a635e64a9053e
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2024-06-03 05:12:12 +0200

    fix(xml): add missing exports for `cdata()` and `comment()` in `mod.ts`

--- xml/mod.ts
+++ xml/mod.ts
@@ -1,3 +1,3 @@
 export { type options as parse_options, parse } from "./parse.ts"
-export { type options as stringify_options, stringify } from "./stringify.ts"
+export { type options as stringify_options, stringify, cdata, comment } from "./stringify.ts"
 export type * from "./_types.ts"
