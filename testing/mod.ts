74e437b030229db0cdb88d92287ae13bd88e3f9c
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2024-07-01 06:45:12 +0200

    feat(testing): add `TestingError`

--- testing/mod.ts
+++ testing/mod.ts
@@ -5,3 +5,6 @@ export { test } from "./_testing.ts"
 /** Alias for `any` that can be used for testing. */
 //deno-lint-ignore no-explicit-any
 export type testing = any
+
+/** TestingError can be used to test expected error behaviours in tests. */
+export class TestingError extends Error {}
