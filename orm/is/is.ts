2b880649ee2dd961383538023dbc67dece9aeecd
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2024-07-03 01:06:37 +0200

    feat(orm): add `is` validation based on `zod`

---
+++ orm/is/is.ts
@@ -0,0 +1,59 @@
+// Imports
+import { z as is } from "npm:zod"
+import { type ErrorMessageOptions, generateErrorMessage } from "npm:zod-error"
+
+/** Original methods. */
+const { parse, parseAsync } = is.ZodType.prototype
+
+/** Error formatter. */
+const format = {
+  prefix: "Validation failed\n  - ",
+  code: {
+    enabled: true,
+    label: null,
+    transform: ({ value }) => `[${value.toLocaleUpperCase()}]`,
+  },
+  delimiter: {
+    component: " ",
+    error: "\n  - ",
+  },
+  message: {
+    enabled: true,
+    label: null,
+    transform: ({ value }) => value.toLocaleLowerCase(),
+  },
+  path: {
+    enabled: true,
+    label: null,
+    type: "objectNotation",
+  },
+} as ErrorMessageOptions
+
+/** Zod parse override. */
+is.ZodType.prototype.parse = function (...args: Parameters<typeof parse>) {
+  try {
+    return parse.apply(this, args)
+  } catch (error) {
+    if (error instanceof is.ZodError) {
+      // deno-lint-ignore no-ex-assign
+      error = new TypeError(generateErrorMessage(error.issues, format))
+    }
+    throw error
+  }
+}
+
+/** Zod parseAsync override. */
+is.ZodType.prototype.parseAsync = async function (...args: Parameters<typeof parseAsync>) {
+  try {
+    return await parseAsync.apply(this, args)
+  } catch (error) {
+    if (error instanceof is.ZodError) {
+      // deno-lint-ignore no-ex-assign
+      error = new TypeError(generateErrorMessage(error.issues, format))
+    }
+    throw error
+  }
+}
+
+/** Input validation. */
+export { is }
---
+++ orm/is/mod.ts
@@ -0,0 +1,9 @@
+
+/**
+ * Validation utilities.
+ *
+ * It contains a {@link https://github.com/colinhacks/zod | Zod} wrapper with human-readable error formatting.
+ *
+ * @module
+ */
+export { is } from "./is.ts"
