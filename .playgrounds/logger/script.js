2dc855242de1c7c606acc09f855434be4916dc4e
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2024-07-13 21:23:06 +0200

    fix: playground update logger example

--- .playgrounds/logger/script.js
+++ .playgrounds/logger/script.js
@@ -2,7 +2,7 @@
   await llibs.forms.setup({
     deno: `
 import { Logger } from "jsr:@libs/logger"
-const log = new Logger({ level: Logger.level.debug, options: { date: true, time: true, caller: { file: true, name: true, line: true } } })
+const log = new Logger({ level: "trace", options: { date: true, time: true, caller: true } })
 
 for (const level of Object.keys(Logger.level)) {
   if (level === "disabled") {
