6e9330df1c05228677ec22543d15a0c7fe9920f9
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2024-09-28 05:09:57 -0400

    feat(diff)!: support deno 2

--- diff/diff_test.ts
+++ diff/diff_test.ts
@@ -8,62 +8,62 @@ async function read(test: string) {
   return { a: a.replaceAll("\r\n", "\n"), b: b.replaceAll("\r\n", "\n"), c: c.replaceAll("\r\n", "\n") }
 }
 
-test("deno")("diff() handles empty texts", async () => {
+test("deno")("`diff()` handles empty texts", async () => {
   const { a, b, c } = await read("empty")
   expect(diff(a, b)).toStrictEqual(c)
 }, { permissions: { read: true } })
 
-test("deno")("diff() handles empty unchanged texts", async () => {
+test("deno")("`diff()` handles empty unchanged texts", async () => {
   const { a, b, c } = await read("identical")
   expect(diff(a, b)).toStrictEqual(c)
 }, { permissions: { read: true } })
 
-test("deno")("diff() handles final newline in texts", async () => {
+test("deno")("`diff()` handles final newline in texts", async () => {
   const { a, b, c } = await read("newline")
   expect(diff(a, b)).toStrictEqual(c)
 }, { permissions: { read: true } })
 
-test("deno")("diff() handles single addition", async () => {
+test("deno")("`diff()` handles single addition", async () => {
   const { a, b, c } = await read("single/addition")
   expect(diff(a, b)).toStrictEqual(c)
 }, { permissions: { read: true } })
 
-test("deno")("diff() handles additions", async () => {
+test("deno")("`diff()` handles additions", async () => {
   const { a, b, c } = await read("addition")
   expect(diff(a, b)).toStrictEqual(c)
 }, { permissions: { read: true } })
 
-test("deno")("diff() handles single deletion", async () => {
+test("deno")("`diff()` handles single deletion", async () => {
   const { a, b, c } = await read("single/deletion")
   expect(diff(a, b)).toStrictEqual(c)
 }, { permissions: { read: true } })
 
-test("deno")("diff() handles deletions", async () => {
+test("deno")("`diff()` handles deletions", async () => {
   const { a, b, c } = await read("deletion")
   expect(diff(a, b)).toStrictEqual(c)
 }, { permissions: { read: true } })
 
-test("deno")("diff() handles single edition", async () => {
+test("deno")("`diff()` handles single edition", async () => {
   const { a, b, c } = await read("single/edition")
   expect(diff(a, b)).toStrictEqual(c)
 }, { permissions: { read: true } })
 
-test("deno")("diff() handles editions", async () => {
+test("deno")("`diff()` handles editions", async () => {
   const { a, b, c } = await read("edition")
   expect(diff(a, b)).toStrictEqual(c)
 }, { permissions: { read: true } })
 
-test("deno")("diff() handles multiple hunks", async () => {
+test("deno")("`diff()` handles multiple hunks", async () => {
   const { a, b, c } = await read("hunks")
   expect(diff(a, b)).toStrictEqual(c)
 }, { permissions: { read: true } })
 
-test("deno")("diff() handles moved lines", async () => {
+test("deno")("`diff()` handles moved lines", async () => {
   const { a, b, c } = await read("moved")
   expect(diff(a, b)).toStrictEqual(c)
 }, { permissions: { read: true } })
 
-test("deno")("diff() handles complex texts", async () => {
+test("deno")("`diff()` handles complex texts", async () => {
   const { a, b, c } = await read("lorem")
   expect(diff(a, b)).toStrictEqual(c)
 }, { permissions: { read: true } })
