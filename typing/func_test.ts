27357e730a7ef46d844fa6fdc2cd89f6b6ac649b
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2024-09-28 02:19:48 -0400

    feat(typing)!: support for deno 2

--- typing/func_test.ts
+++ typing/func_test.ts
@@ -1,7 +1,7 @@
 import { AsyncFunction, AsyncGeneratorFunction, GeneratorFunction } from "./func.ts"
 import { expect, test } from "@libs/testing"
 
-test("all")("new AsyncFunction() returns an async function", async () => {
+test("all")("`new AsyncFunction()` returns an async function", async () => {
   const func = new AsyncFunction("return 1")
   const ret = func()
   expect(func).toBeInstanceOf(Function)
@@ -9,7 +9,7 @@ test("all")("new AsyncFunction() returns an async function", async () => {
   await expect(ret).resolves.toBe(1)
 })
 
-test("all")("new GeneratorFunction() returns a generator function", () => {
+test("all")("`new GeneratorFunction()` returns a generator function", () => {
   const func = new GeneratorFunction("yield 1")
   const gen = func()
   expect(func).toBeInstanceOf(Function)
@@ -20,7 +20,7 @@ test("all")("new GeneratorFunction() returns a generator function", () => {
   expect(ret).toEqual({ value: 1, done: false })
 })
 
-test("all")("new AsyncGeneratorFunction() returns an async generator function", async () => {
+test("all")("`new AsyncGeneratorFunction()` returns an async generator function", async () => {
   const func = new AsyncGeneratorFunction("yield 1")
   const gen = func()
   expect(func).toBeInstanceOf(Function)
