bad79cf15987d1c75feabdee28fa8715f43e87c0
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2024-11-28 02:34:04 -0500

    fix(typing): update tests

--- typing/_func_test.ts
+++ typing/_func_test.ts
@@ -1,7 +1,7 @@
 import { functions } from "./_func.ts"
 import { expect, test } from "@libs/testing"
 
-test("all")("`functions` is a dictionary of functions", () => {
+test("`functions` is a dictionary of functions", () => {
   expect(functions.async).toBeInstanceOf(Function)
   expect(functions.generator).toBeInstanceOf(Function)
   expect(functions.asyncGenerator).toBeInstanceOf(Function)
