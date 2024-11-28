c007a3ad49920f021985e73a5da6a3cd87d916c2
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2024-11-28 17:47:26 -0500

    fix(testing): bun not registering properly test
    https://github.com/oven-sh/bun/issues/11660

--- testing/_testing/bun.ts
+++ testing/_testing/bun.ts
@@ -1,19 +1,35 @@
 // Imports
 import type { Promisable, runner } from "./common.ts"
 import { format } from "./common.ts"
-const bun = await import(`${"bun"}:test`)
+
+// deno-lint-ignore no-explicit-any
+const bun = (globalThis as any).Bun
 
 /** Bun test runner. */
 export const test = Object.assign(function (name: string, fn: () => Promisable<void>) {
-  return bun.test(format(name), fn)
+  return bun.jest(caller()).test(format(name), fn)
 }, {
   only: function (name: string, fn: () => Promisable<void>) {
-    return bun.test.only(format(name), fn)
+    return bun.jest(caller()).test.only(format(name), fn)
   },
   skip: function (name: string, fn: () => Promisable<void>) {
-    return bun.test.skip(format(name), fn)
+    return bun.jest(caller()).test.skip(format(name), fn)
   },
   todo: function (name: string, fn: () => Promisable<void>) {
-    return bun.test.todo(format(name), fn)
+    return bun.jest(caller()).test.todo(format(name), fn)
   },
 }) as runner
+
+/** Retrieve caller test file. */
+function caller() {
+  const Trace = Error as unknown as { prepareStackTrace: (error: Error, stack: CallSite[]) => unknown }
+  const _ = Trace.prepareStackTrace
+  Trace.prepareStackTrace = (_, stack) => stack
+  const { stack } = new Error()
+  Trace.prepareStackTrace = _
+  const caller = (stack as unknown as CallSite[])[2]
+  return caller.getFileName().replaceAll("\\", "/")
+}
+
+/** V8 CallSite (subset). */
+type CallSite = { getFileName: () => string }
