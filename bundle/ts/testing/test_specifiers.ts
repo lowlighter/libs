3b2de6c17d1484ac149dab845d27ef79c53e3423
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2024-12-16 03:04:52 -0500

    fix(bundle): update test

--- bundle/ts/testing/test_specifiers.ts
+++ bundle/ts/testing/test_specifiers.ts
@@ -1,7 +1,6 @@
 // deno-lint-ignore-file no-console no-external-import
 // Example module with specifiers
 import { red } from "jsr:@std/fmt/colors"
-import zod from "npm:zod"
 import { inspect } from "node:util"
-console.log(red, zod, inspect)
+console.log(red, inspect)
 console.log("success" as string)
