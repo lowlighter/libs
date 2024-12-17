1887265483378f2ec4af252e8ca601491f2e27a3
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2024-12-17 00:01:50 -0500

    feat(git): add package (#93)

---
+++ git/changelog.ts
@@ -0,0 +1,68 @@
+// Imports
+import type { Arrayable, Nullable } from "@libs/typing"
+import { assert } from "@std/assert"
+import * as semver from "@std/semver"
+import * as git from "./mod.ts"
+export type { Arrayable, Nullable }
+export type * from "@std/semver"
+
+/**
+ * Generate a changelog based on the commits since the last version bump.
+ *
+ * It is automatically determined by the list of commits since the last edit of the "version" key.
+ * Commits must follow the conventional commits syntax (non-conventional commits are ignored).
+ */
+export function changelog(path: string, { scopes = [], minor = ["feat"], patch = ["fix", "docs", "perf", "refactor", "chore"], ...options } = {} as ChangelogOptions): Changelog {
+  // Find the base commit (last version bump)
+  const base = git.blame(path).find(({ content }) => /"version": "\d\.\d\.\d.*"/.test(content))
+  assert(base, `Could not find "version" key in ${path}`)
+
+  // Parse the version
+  const version = semver.parse(base.content.match(/"version": "(?<version>\d\.\d\.\d.*)"/)?.groups?.version ?? "")
+  const commits = git.log(base.sha, { filter: { conventional: true, scopes } })
+  const result = { version: { bump: null, current: version, next: version }, changelog: "" } as Changelog
+
+  // Bump the version
+  if (commits.some(({ breaking }) => breaking)) {
+    result.version.bump = "major"
+    result.version.next = semver.increment(version, result.version.bump)
+  } else if (commits.some(({ type }) => minor.includes(type as string))) {
+    result.version.bump = "minor"
+    result.version.next = semver.increment(version, result.version.bump)
+  } else if (commits.some(({ type }) => patch.includes(type as string))) {
+    result.version.bump = "patch"
+    result.version.next = semver.increment(version, result.version.bump)
+  }
+
+  // Write the new version
+  if (options.write) {
+    const content = Deno.readTextFileSync(path)
+    Deno.writeTextFileSync(path, content.replace(/"version": "\d\.\d\.\d.*"/, `"version": "${semver.format(result.version.next)}"`))
+  }
+
+  // Generate the changelog
+  result.changelog = commits.map(({ summary }) => summary).join("\n")
+  return result
+}
+
+/** Options for {@linkcode changelog()}. */
+export type ChangelogOptions = {
+  /** The scopes to filter. */
+  scopes?: Arrayable<string>
+  /** The type of commits that increase the "patch" component of semver. */
+  patch?: Arrayable<string>
+  /** The type of commits that increase the "minor" component of semver. */
+  minor?: Arrayable<string>
+  /** Whether to update the version in the file. */
+  write?: boolean
+}
+
+/** Changelog. */
+export type Changelog = {
+  version: {
+    bump: Nullable<"major" | "minor" | "patch">
+    current: semver.SemVer
+    next: semver.SemVer
+  }
+  changelog: string
+}
---
+++ git/log_test.ts
@@ -0,0 +1,57 @@
+import { log, type LogEntry } from "./log.ts"
+import { expect, test } from "@libs/testing"
+
+const expected = [
+  {
+    sha: "a".repeat(40),
+    author: "foo",
+    time: 1,
+    conventional: true,
+    type: "chore",
+    scopes: [],
+    breaking: false,
+    subject: "initial commit",
+    summary: "chore: initial commit",
+  },
+  {
+    sha: "b".repeat(40),
+    author: "bar",
+    time: 2,
+    conventional: true,
+    type: "fix",
+    scopes: ["a", "b"],
+    breaking: false,
+    subject: "add a comment",
+    summary: "fix(a, b): add a comment",
+  },
+  {
+    sha: "c".repeat(40),
+    author: "baz",
+    time: 3,
+    conventional: true,
+    type: "fix",
+    scopes: ["c"],
+    breaking: true,
+    subject: "fix a bug",
+    summary: "fix(c)!: fix a bug",
+  },
+  {
+    sha: "d".repeat(40),
+    author: "qux",
+    time: 4,
+    conventional: false,
+    type: null,
+    scopes: [],
+    breaking: null,
+    subject: "random commit",
+    summary: "random commit",
+  },
+] as LogEntry[]
+
+function format(entries: LogEntry[]) {
+  return entries.map((entry) => `<<${entry.sha}>> <<${entry.time}>> <<${entry.author}>> ${entry.conventional ? `${entry.type}${entry.scopes?.length ? `(${entry.scopes.join(", ")})` : ""}${entry.breaking ? "!" : ""}: ${entry.subject}` : entry.subject}`).join("\n")
+}
+
+test("`log()` parses `git log --pretty=<<%H>> <<%at>> <<%an>> %s`", () => {
+  expect(log("", { stdout: format(expected) })).toEqual(expected)
+})
