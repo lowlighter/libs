2e2dcb2c8aabd52e4fd69c98bcd6474e61d9f3d1
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2025-10-02 18:09:08 -0400

    chore(orm): deprecate package

--- orm/is/mod.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Validation utilities.
- *
- * It contains a {@link https://github.com/colinhacks/zod | Zod} wrapper with human-readable error formatting.
- *
- * @module
- */
-export { is, schema } from "./is.ts"
