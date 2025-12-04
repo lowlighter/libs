c51e66af17cb7538dd76f971bcdfb9adea7e47cc
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2025-12-04 13:15:54 -0500

    feat(testing): add inspect helper in highlighting

--- testing/highlight_test.ts
+++ testing/highlight_test.ts
@@ -1,7 +1,7 @@
 import { test } from "./test.ts"
 import { expect } from "./expect.ts"
-import { highlight } from "./highlight.ts"
-import { bgWhite, black, gray, green, underline, yellow } from "@std/fmt/colors"
+import { highlight, inspect } from "./highlight.ts"
+import { bgWhite, black, gray, green, stripAnsiCode, underline, yellow } from "@std/fmt/colors"
 
 test("`highlight()` colors code string with ansi", () => {
   expect(highlight("`// foo`")).toBe(gray("// foo"))
@@ -20,3 +20,14 @@ test("`highlight()` supports type option", () => {
   expect(highlight("`'foo'`", { type: "warn" })).toBe(yellow("'foo'"))
   expect(highlight("`'foo'`", { type: "debug" })).toBe(gray("'foo'"))
 })
+
+for (
+  const { inspected, expected } of [
+    { inspected: { foo: "bar" }, expected: '{ foo: "bar" }' },
+    { inspected: function () {}, expected: "fn" },
+  ] as const
+) {
+  test(`\`inspect(${Deno.inspect(inspected)})\` returns \`${expected}\``, () => {
+    expect(stripAnsiCode(inspect(inspected))).toBe(expected)
+  })
+}
