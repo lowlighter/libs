1cbbd3b8cf8a38b707112c767be541f44489ee83
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2026-01-21 18:27:57 -0500

    Update promises.ts

--- toolbox/promises.ts
+++ toolbox/promises.ts
@@ -1,5 +1,5 @@
 /** Returns the current state of a promise. */
-export async function state(promise: Promise<unknown>) {
+export async function state(promise: Promise<unknown>): Promise<"pending" | "fulfilled" | "rejected"> {
   const check = new Promise<void>((resolve) => setTimeout(resolve, 0))
   try {
     return await Promise.race([promise.then((_) => "fulfilled" as const).catch((_) => "rejected" as const), check.then((_) => "pending" as const)])
