6879a8b8365050ffafdb1870ecf8240ed575a67a
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2026-04-22 16:32:04 -0400

    fix(toolbox): missing typings

--- toolbox/format.ts
+++ toolbox/format.ts
@@ -13,16 +13,16 @@ export function countryFlag(code: string): string {
 }
 
 /** Normalize string to uppercase and remove diacritics. */
-export function unfd(string: string) {
+export function unfd(string: string): string {
   return string.toLocaleUpperCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")
 }
 
 /** Normalize string to lowercase and remove diacritics. */
-export function lnfd(string: string) {
+export function lnfd(string: string): string {
   return unfd(string).toLocaleLowerCase()
 }
 
 /** Strip emojis from a string. */
-export function stripEmojis(string: string) {
+export function stripEmojis(string: string): string {
   return string.replace(/[\p{Emoji}\p{Emoji_Component}]+/gu, "")
 }
